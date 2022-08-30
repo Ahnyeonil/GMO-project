c4ea1d3df50abef9ac98e1126bbd933a6b91fdc1
kimyuntae19 <kimyuntae19@gmail.com>
2022-08-31 01:43:50 +0900

    완성인데 완성아닌 완성

--- static/scripts.js
+++ static/scripts.js
@@ -175,31 +175,3 @@ function simple_detail_call(title) {
     })
 }
 
-
-// 지영님 js
-
-const goToMain = () => {
-    document.location.href = '/';
-}
-
-const goToSokchoIntro = () => {
-    document.location.href = '/sokcho/intro';
-    // location.replace('/sokcho/intro');
-}
-
-const goToReviewList = () => {
-    document.location.href = '/post';
-}
-
-const dropdownMenu = document.querySelector(".dropdown-menu");
-const dropdownButton = document.querySelector(".dropdown-button");
-
-dropdownButton.addEventListener("click", function (event) {
-    if (this.active) {
-        dropdownMenu.classList.remove("active");
-    } else {
-        dropdownMenu.classList.add("active");
-    }
-
-    this.active = !this.active;
-});
