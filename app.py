df9b2ffd64af8e09d245c33a717571404180f34e
Kyeil <meme951023@gmail.com>
2022-09-01 00:19:25 +0900

    포스팅 목록 및 포스팅

--- app.py
+++ app.py
@@ -281,5 +281,48 @@ def comment_ayi():
 
     return jsonify({'msg':'등록 완료!'})
 
+##포스팅 추가 Post
+@app.route("/postinglist", methods=["POST"])
+def postinglist_post():
+    picture_receive = request.form['picture_give']
+    name_receive = request.form['name_give']
+    title_receive = request.form['title_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    print(name_receive)
+
+    doc = {
+        'picture' : picture_receive,
+        'name': name_receive,
+        'title': title_receive,
+        'star': star_receive,
+        'comment': comment_receive
+    }
+
+    db.posting.insert_one(doc)
+
+    return jsonify({'msg': 'POST(속초 포스팅) 연결 완료!'})
+
+##포스팅 추가 Get
+@app.route("/posting", methods=["GET"])
+def posting_get():
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+
+##포스팅 목록
+@app.route("/postinglist", methods=["GET"])
+def postinglist_get():
+    posting_list = list(db.posting.find({}, {'_id': False}))
+
+    return jsonify({'postings': posting_list})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+
+
+
+
